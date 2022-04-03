54351247a98f78218ebcf7e8142a5dd39529c1bb
ltrafecanty <ltrafeca@ucsc.edu>
2022-04-03 15:37:35 -0400

    Unit 1

---
+++ PrimitiveTypes/Ex1.java
@@ -0,0 +1,16 @@
+public class Ex1{
+    public static void main(String[] args){
+        int a = 23;
+        int b = 51;
+        System.out.println(a/b);
+
+
+        byte c = 8; 
+        byte d = 3;
+        int res2 = c % d;
+        System.out.println(res2);
+
+        int result = 8 % 3 / 2 + 9;
+        System.out.println(result);
+    }
+}
---
+++ PrimitiveTypes/Ex2.java
@@ -0,0 +1,19 @@
+public class Ex2 {
+    public static void main(String[] args) {
+        float f1 = 20.2355235123555f;
+        double d1 = 20.2355235123555;
+        System.out.println(f1);
+        System.out.println(d1);
+
+        char [] ch = {'h','e','l','l','o'};
+
+        boolean b;
+        String str = "Welcome to Udemy";
+
+        b = (str.length() == 14);
+        System.out.println(b);
+        
+
+
+    }
+}
